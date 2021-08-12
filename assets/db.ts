a34c07d5a8eb69d9c1732f1594785b05976918c7
lakai4eg <perm.lakai@gmail.com>
2021-08-12 14:16:08 +0500

    Auto-generated commit [ci skip]

---
+++ assets/db.ts
@@ -0,0 +1,124 @@
+let db = [
+  {
+    id: 0,
+    name: 'Аитова Аниса Равильевна',
+    date: '2021-08-12',
+    workTimeStart: '09:00',
+    workTimeEnd: '12:30',
+    busyInterval: [{time: '10:10', name: 'ОченьДлиннаяФамилия Петр Петрович'}, {time: '10:30', name:'Иванов Иван Иванович'}]
+  },
+  {
+    id: 0,
+    name: 'Аитова Аниса Равильевна',
+    date: '2021-08-13',
+    workTimeStart: '13:30',
+    workTimeEnd: '15:30',
+    busyInterval: []
+  },
+  {
+    id: 1,
+    name: 'Вавилов Никита Васильевич',
+    date: '2021-08-12',
+    workTimeStart: '10:30',
+    workTimeEnd: '15:30',
+    busyInterval: []
+  },
+  {
+    id: 1,
+    name: 'Вавилов Никита Васильевич',
+    date: '2021-08-13',
+    workTimeStart: '14:30',
+    workTimeEnd: '15:30',
+    busyInterval: []
+  },
+  {
+    id: 2,
+    name: 'Гилева Ирина Сергеевна',
+    date: '2021-08-12',
+    workTimeStart: '13:30',
+    workTimeEnd: '15:30',
+    busyInterval: []
+  },
+  {
+    id: 3,
+    name: 'Ковалева Наталья Васильевна',
+    date: '2021-08-12',
+    workTimeStart: '14:00',
+    workTimeEnd: '16:00',
+    busyInterval: [{time: '14:50', name: 'ОченьДлиннаяФамилия Петр Петрович'}, {time: '14:40', name:'Иванов Иван Иванович'}]
+  },
+  {
+    id: 4,
+    name: 'Маматова Евгения Александровна',
+    date: '2021-08-12',
+    workTimeStart: '11:30',
+    workTimeEnd: '17:00',
+    busyInterval: [{time: '13:50', name: 'ОченьДлиннаяФамилия Петр Петрович'}, {time: '14:40', name:'Иванов Иван Иванович'}]
+  },
+  {
+    id: 5,
+    name: 'Опарина Ольга Александровна',
+    date: '2021-08-12',
+    workTimeStart: '12:30',
+    workTimeEnd: '15:00',
+    busyInterval: [{time: '13:50', name: 'ОченьДлиннаяФамилия Петр Петрович'}, {time: '14:40', name:'Иванов Иван Иванович'}]
+  },
+  {
+    id: 6,
+    name: 'Тихомирова Нелли Григорьевна',
+    date: '2021-08-12',
+    workTimeStart: '13:30',
+    workTimeEnd: '16:00',
+    busyInterval: [{time: '13:50', name: 'ОченьДлиннаяФамилия Петр Петрович'}, {time: '14:40', name:'Иванов Иван Иванович'}]
+  },
+  {
+    id: 7,
+    name: 'Ширинкина Антонида Анатольевна',
+    date: '2021-08-12',
+    workTimeStart: '11:30',
+    workTimeEnd: '16:00',
+    busyInterval: [{time: '13:50', name: 'ОченьДлиннаяФамилия Петр Петрович'}, {time: '14:40', name:'Иванов Иван Иванович'}]
+  },
+  {
+  id: 3,
+  name: 'Ковалева Наталья Васильевна',
+  date: '2021-08-13',
+  workTimeStart: '13:00',
+  workTimeEnd: '16:00',
+    busyInterval: [{time: '13:50', name: 'ОченьДлиннаяФамилия Петр Петрович'}, {time: '14:40', name:'Иванов Иван Иванович'}]
+},
+{
+  id: 4,
+    name: 'Маматова Евгения Александровна',
+  date: '2021-08-13',
+  workTimeStart: '11:30',
+  workTimeEnd: '17:00',
+  busyInterval: [{time: '13:50', name: 'ОченьДлиннаяФамилия Петр Петрович'}, {time: '14:40', name:'Иванов Иван Иванович'}]
+},
+{
+  id: 5,
+    name: 'Опарина Ольга Александровна',
+  date: '2021-08-13',
+  workTimeStart: '12:30',
+  workTimeEnd: '15:00',
+  busyInterval: [{time: '13:50', name: 'ОченьДлиннаяФамилия Петр Петрович'}, {time: '14:40', name:'Иванов Иван Иванович'}]
+},
+{
+  id: 6,
+    name: 'Тихомирова Нелли Григорьевна',
+  date: '2021-08-13',
+  workTimeStart: '13:30',
+  workTimeEnd: '16:00',
+  busyInterval: [{time: '13:50', name: 'ОченьДлиннаяФамилия Петр Петрович'}, {time: '14:40', name:'Иванов Иван Иванович'}]
+},
+{
+  id: 7,
+    name: 'Ширинкина Антонида Анатольевна',
+  date: '2021-08-13',
+  workTimeStart: '11:30',
+  workTimeEnd: '16:00',
+  busyInterval: [{time: '13:50', name: 'ОченьДлиннаяФамилия Петр Петрович'}, {time: '14:40', name:'Иванов Иван Иванович'}]
+},
+]
+
+export default db;
